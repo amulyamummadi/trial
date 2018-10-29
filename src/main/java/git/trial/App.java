79935ac45ccd4dc92b59a424386b3d1f92d15f95
Mummadi Amulya <amulya.mummadi@atmecs.com>
2018-10-29 16:55:34 +0530

    added testng.xml

--- src/main/java/git/trial/App.java
+++ src/main/java/git/trial/App.java
@@ -7,13 +7,9 @@ import org.testng.annotations.Test;
  *
  */
 public class App {
-<<<<<<< HEAD
+
 	@Test
 	public void checking() {
 		System.out.println("entered into @test");
-=======
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
->>>>>>> 155dde0cfcc2f14db75d71310562839f913b15a4
 	}
 }
